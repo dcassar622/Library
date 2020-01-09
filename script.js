ba236bb8f054d26e118698b32634fbc964d22cd5
ericksen-github <ericksen.github@gmail.com>
2020-01-09 17:32:38 +0100

    Form shows/hides correctly. Need to adjust background on form. Correctly adds book to library/table.

--- script.js
+++ script.js
@@ -9,8 +9,46 @@ function Book(title, author, pages, read) {
 	this.read = read;
 }
 
+function getBookInfo() {
+    const titleText = document.getElementById("titleText").value; 
+    const authorText = document.getElementById("authorText").value; 
+    const pagesText = document.getElementById("pagesText").value; 
+    const e = document.getElementById("selectMenu");
+    const readText = e.options[e.selectedIndex].value; 
+
+    if (!checkBoxValues(titleText, authorText, pagesText)) {
+        return; 
+    }
+
+    addBookToLibrary(new Book(titleText, authorText, pagesText, readText))
+}
+
+function checkBoxValues(titleText, authorText, pagesText) {
+    let checker = true; 
+    if (titleText == "" || titleText == null) {
+        document.getElementById("titleText").style.border = "solid 1px red"; 
+        checker = false; 
+    } else { 
+        document.getElementById("titleText").style.border = "solid 1px black"; 
+    }
+    if (authorText == "" || authorText == null) {
+        document.getElementById("authorText").style.border = "solid 1px red"; 
+        checker = false;  
+    } else { 
+        document.getElementById("authorText").style.border = "solid 1px black"; 
+    }
+    if (pagesText == "" || pagesText == null || pagesText <= 0 || isNaN(pagesText)) {
+        document.getElementById("pagesText").style.border = "solid 1px red"; 
+        checker = false; 
+    } else { 
+        document.getElementById("pagesText").style.border = "solid 1px black"; 
+    }
+    return checker; 
+}
+
 function addBookToLibrary(book){
     myLibrary.push(book); 
+    hideForm(); 
     render(myLibrary); 
 }
 
@@ -26,8 +64,31 @@ function render(myLibrary) {
                          <td></td>
                     `;
     }
-
     tbody.innerHTML = bookHTML; 
+}
+
+function showForm() {
+    const inputForm = document.getElementById("inputForm"); 
+    const mainBody = document.getElementById("mainBody"); 
+
+    inputForm.style.left = (window.innerWidth / 2 - 165) + "px"; 
+    inputForm.style.display = "block";
+    mainBody.style.backgroundColor = "black"; 
+}
+
+// for the cancel button
+function hideForm() {
+    const inputForm = document.getElementById("inputForm"); 
+    const mainBody = document.getElementById("mainBody"); 
+
+    inputForm.style.display = "none";
+    mainBody.style.backgroundColor = "white"; 
+}
+
+// hides input form on page load
+window.onload = () => {
+    const inputForm = document.getElementById("inputForm"); 
+    inputForm.style.display = "none"; 
 }
 
 addBookToLibrary(new Book("1984", "George Orwell", 328, "Read"));
