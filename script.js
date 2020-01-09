e8bf13e5d30577dd061dafd676e20cf28439a171
ericksen-github <ericksen.github@gmail.com>
2020-01-09 12:39:56 +0100

    Have tables up and running.

--- script.js
+++ script.js
@@ -1,50 +1,37 @@
-function Book(title, author, pages, haveRead) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function() {
-        if (read == true) {
-            return title + " by " + author + ", " + pages + " pages, " + "have read."; 
-        } else {
-            return title + " by " + author + ", " + pages + " pages, " + "have not read.";
-        }
-    }
-}
-
-function addBookToLibrary() {
-
-}
-
-function generateTableHead(table) {
-    let thead = table.createTHead();
-    let row = thead.insertRow(); 
-
-    for (let key of data) {
-        let th = document.createElement("th");
-        let text = document.createTextNode(key);
-        th.appendChild(text);
-        row.appendChild(th);
-    }
-}
-
-function generateTable(table, mountains) {
-    for (let element of mountains) {
-        let row = table.insertRow(); 
-        for (key in element) {
-            let cell = row.insertCell();
-            let text = document.createTextNode(element[key]); 
-            cell.appendChild(text); 
-        }
-    }
-}
-
-let mountains = [
-    { name: "Monte Falco", height: 1658, place: "Parco Foreste Casentinesi" },
-    { name: "Monte Falterona", height: 1654, place: "Parco Foreste Casentinesi" }
-  ];
-
+let myLibrary = [];
 let table = document.querySelector("table"); 
-let data = ["Title", "Author", "Pages", "Status"];
-generateTableHead(table, data);
-generateTable(table, mountains); 
+let tableHeaders = ["Title", "Author", "Pages", "Status", ""];
+
+function Book(title, author, pages, read) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+}
+
+function addBookToLibrary(book){
+    myLibrary.push(book); 
+    render(myLibrary); 
+}
+
+function render(myLibrary) {
+    let tbody = document.querySelector("tbody"); 
+    let bookHTML = ""; 
+
+    for (let book of myLibrary) {
+        bookHTML += `<tr><td>${book.title}</td>
+                         <td>${book.author}</td>
+                         <td>${book.pages}</td>
+                         <td>${book.read}</td>
+                         <td></td>
+                    `;
+    }
+
+    tbody.innerHTML = bookHTML; 
+}
+
+addBookToLibrary(new Book("1984", "George Orwell", 328, "Read"));
+addBookToLibrary(new Book("The Hobbit", "J.R.R. Tolkien", 295, "Read"));
+addBookToLibrary(new Book("The Martian", "Andy Weir", 369, "Read"));
+
+render(myLibrary); 
